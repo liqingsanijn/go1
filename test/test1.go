739ca5ae4299c74cbaf88eea1c8da720be7c570b
jingx <1270858488@qq.com>
2017-03-30 13:51:26 +0800

    test/test2.go

--- test/test1.go
+++ test/test1.go
@@ -5,11 +5,11 @@ func main() {
 	fmt.Println("hello,world")
 	a := 1
 	fmt.Println(a)
-	b := new(test)
+	var b Test
  	b.test1 = 0
 	fmt.Println(b.test1)	
 }
-type test struct {
+type Test struct {
 	test1 int
 	test2 int
 }
