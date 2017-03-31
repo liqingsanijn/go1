0f6c43bfa97da826698cb1944753b4a034683297
jingx <1270858488@qq.com>
2017-03-31 10:09:42 +0800

    for and printf

--- test/test2.go
+++ test/test2.go
@@ -6,5 +6,15 @@ import (
 func main() {
 	var a bool
 	fmt.Println(a)
-
+	testMap := make(map[string]string)
+	testMap["key1"] = "value1"
+	testMap["key2"] = "value2"
+	for key,value := range testMap {
+		fmt.Println("key:" + key + ",value:" + value)
+	}
+	arr := [...]int{1, 2, 3}
+	for i,v := range arr {
+		fmt.Printf("i:%d,v:%d\n", i, v)
+	}
+	fmt.Println("a", "b", "c")
 }
