da39b0d771622ac7a32d0230fe08331f48f06e71
jingx <1270858488@qq.com>
2017-03-31 16:40:57 +0800

    测试函数

--- test/test3.go
+++ test/test3.go
@@ -13,4 +13,16 @@ func main() {
 	)
 	fmt.Println("test3")
 	fmt.Println(a, b, c, d)
+	test3()
+	e, f := test4()
+	fmt.Println(e, f)
+}
+
+func test3()  {
+	fmt.Println("函数test3")
+}
+
+func test4() (int, int) {
+	fmt.Println("函数test4")
+	return 1, 2
 }
